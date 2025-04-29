9d986d586f05ebc980208daf1f50df697d5b8036
Narita Ryosuke <ryo13chan@gmail.com>
2025-04-29 15:10:23 +0900

    format

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,23 +1,23 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
-  "addons": [
+  addons: [
     {
-      "name": "@storybook/addon-essentials",
-      "options": {
-        "docs": false
-      }
+      name: '@storybook/addon-essentials',
+      options: {
+        docs: false,
+      },
     },
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions"
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
-};
-export default config;
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+}
+export default config

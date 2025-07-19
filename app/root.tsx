bb7711c1d0236d12a1d230b5fbf8abd39c78d8f2
Narita Ryosuke <ryo13chan@gmail.com>
2025-07-19 15:51:16 +0900

    Tailwind CSSが効かない不具合の修正

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import {
-  // Links,
+  Links,
   Meta,
   Outlet,
   Scripts,
@@ -11,19 +11,6 @@ import { ThemeProvider } from '~/components/ThemeProvider'
 import type { Route } from './+types/root'
 import './app.css'
 
-// export const links: Route.LinksFunction = () => [
-//   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-//   {
-//     rel: 'preconnect',
-//     href: 'https://fonts.gstatic.com',
-//     crossOrigin: 'anonymous',
-//   },
-//   {
-//     rel: 'stylesheet',
-//     href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-//   },
-// ]
-
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja" suppressHydrationWarning>
@@ -31,6 +18,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <Links />
       </head>
       <body>
         <ThemeProvider
